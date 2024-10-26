b8fb54cab94ecda5e23aa1815f12b007d88f8170
Bhekti <achmadbhekti@gmail.com>
2024-10-26 19:25:16 +0700

    add pop up in portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,6 +10,8 @@ declare var data: any;
 })
 export class PortfolioComponent implements OnInit {
 	public portfolioData = data['Portfolio'];
+	public isModalOpen = false;  // Untuk kontrol tampilan pop-up
+	public selectedProject: any; // Menyimpan data proyek yang diklik
 
 	constructor(private changeDetectorRef: ChangeDetectorRef) {
 		changeDetectorRef.detach();
@@ -19,4 +21,17 @@ export class PortfolioComponent implements OnInit {
 		this.changeDetectorRef.detectChanges();
 	}
 
+	// Buka pop-up dan simpan data proyek yang diklik
+	openModal(project: any) {
+		this.selectedProject = project;
+		this.isModalOpen = true;
+		this.changeDetectorRef.detectChanges(); // Deteksi perubahan
+	}
+
+	// Tutup pop-up dan reset data proyek
+	closeModal() {
+		this.isModalOpen = false;
+		this.selectedProject = null;
+		this.changeDetectorRef.detectChanges(); // Deteksi perubahan
+	}
 }
