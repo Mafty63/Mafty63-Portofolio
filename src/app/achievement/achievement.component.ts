5ccfa17e26838c474330540d90295723de944c7d
Bhekti <achmadbhekti@gmail.com>
2024-11-13 14:11:37 +0700

    update

--- src/app/achievement/achievement.component.ts
+++ src/app/achievement/achievement.component.ts
@@ -1,21 +1,21 @@
-import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
 
-declare var data : any;
-declare var VanillaTilt:any;
+declare var data: any;
+declare var VanillaTilt: any;
 
 @Component({
 	selector: 'app-achievement',
 	templateUrl: './achievement.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	styleUrls: ['./achievement.component.css']
-
 })
-
-export class AchievementComponent implements OnInit,AfterViewInit {
-	public achievementData  = data['Achievement'];
-	public checkScreenSize : boolean = screen.width >= 768;
+export class AchievementComponent implements OnInit, AfterViewInit {
+	public achievementData = data['Achievement'];
+	public darkTheme: boolean = true; // This can be set dynamically or from user preferences
+	public checkScreenSize: boolean = window.innerWidth >= 768;
 
 	constructor(private changeDetectorRef: ChangeDetectorRef) {
+		// Detach ChangeDetectorRef to optimize performance
 		changeDetectorRef.detach();
 	}
 
@@ -34,5 +34,18 @@ export class AchievementComponent implements OnInit,AfterViewInit {
 				scale: 1.03
 			});
 		}
-    }
+	}
+
+	// Toggle dark theme like in the TrainingComponent
+	toggleDarkTheme(): void {
+		this.darkTheme = !this.darkTheme;
+		this.changeDetectorRef.detectChanges();
+	}
+
+	// Listen for window resize to dynamically update screen size check
+	@HostListener('window:resize', ['$event'])
+	onResize(event: any): void {
+		this.checkScreenSize = window.innerWidth >= 768;
+		this.changeDetectorRef.detectChanges();
+	}
 }
